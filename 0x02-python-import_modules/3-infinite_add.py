d20a10128df19af065bd0f71a3e75cfe60c92299
teamkwkweku <teamkweku@outlook.com>
2023-06-08 17:24:32 +0000

    a program that prints all the result of the addition of argvs

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """ print the total of all commandline args """
+    import sys
+
+    total = 0
+    argc = len(sys.argv) - 1
+    for i in range(argc):
+        total += int(sys.argv[i + 1]) # all argv aside agrv[0]
+    print("{:d}".format(total))
