b981f1d444233ce412b284bc60c93238dd81faf4
teamkweku <teamkweku@outlook.com>
2023-06-11 10:50:25 +0000

    a function that prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """ function that prints a matrix of ints
+        
+        Args:
+            matrix: matrix to be printed
+
+        Returns:
+            return value. prints int in matrix
+    """
+    if matrix == [[]]:
+        print()
+        return
+
+    for row in matrix:
+        for i, element in enumerate(row):
+            print("{:d}".format(element), end="")
+            if i < len(row) - 1:
+                print(" ", end="")
+        print()
+
