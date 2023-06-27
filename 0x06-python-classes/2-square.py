3b54251ddd45c472713bb6dc0e89b7e2e489d3e3
teamkweku <teamkweku@outlook.com>
2023-06-27 15:20:18 +0000

    a class Square that defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This is a module for a class definition of a square"""
+
+
+class Square:
+    """This is an implementation of a class square
+
+    Attributes:
+        size(int): the size of a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Intializes an instance of a square class
+
+        Args:
+            size(int, optional): size of the square (default: size = 0)
+
+        Raises:
+            TypeError: if `size` is not an integer
+            ValueError: if `size` is less than 0
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
