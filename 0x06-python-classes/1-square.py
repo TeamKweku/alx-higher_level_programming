609cfa72470e9390ad7278cada58db893a527d50
teamkweku <teamkweku@outlook.com>
2023-06-27 14:51:53 +0000

    a class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,12 +3,14 @@
 This is a module for a class square
 """
 
-class Square:
-    """
-    This is an implementation of a class square
 
-    Args:
-        size: attribute size of the square class
-    """
+class Square:
+    """This is an implementation of a class square"""
+
     def __init__(self, size):
+        """initialize a new square
+
+        Args:
+            size: The size of a new square
+        """
         self.__size = size
