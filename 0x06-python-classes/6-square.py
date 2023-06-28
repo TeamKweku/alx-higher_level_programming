770acfb297fb02bd6084a2783933e5835579709f
teamkweku <teamkweku@outlook.com>
2023-06-28 20:04:09 +0000

    fix for case of string and negagtive value in tuple

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -109,7 +109,6 @@ class Square:
         """
         if self.__size == 0:
             print("")
-            return
         else:
             for i in range(self.__position[1]):
                 print()
