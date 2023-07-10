d9145a334e8e033528d8a3569bb3318115298336
teamkweku <teamkweku@outlook.com>
2023-07-10 18:53:50 +0000

    a class BaseGeometry (based on 5-base_geometry.py)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a module that defines an empty class BaseGeometry"""
+
+
+class BaseGeometry():
+    """This is the implementation of a class BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
