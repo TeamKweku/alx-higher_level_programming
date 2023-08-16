00e7d6a09b9477ce5efea21b474fb8cf8a9a7108
teamkweku <teamkweku@outlook.com>
2023-08-16 16:08:45 +0000

    a script that lists all privileges of the MySQL users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- script that lists all the privileges of the MySQL users
+-- script that lists all the privileges of the user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
