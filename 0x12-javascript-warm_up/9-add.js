ead179cc346423713cdfcce97397aed8556d59be
teamkweku <teamkweku@outlook.com>
2023-09-11 19:51:34 +0000

    a script that prints the addition of 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const num1 = Number(args[0]);
+const num2 = Number(args[1]);
+
+function add (a, b) {
+  return (a + b);
+}
+
+const sum = add(num1, num2);
+console.log(sum);
