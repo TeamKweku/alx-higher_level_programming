cb0decde7696ffb89edb47a4f683471de40859b6
teamkweku <teamkweku@outlook.com>
2023-09-16 06:48:45 +0000

    a python file that creates a city class and script that queries the class

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,9 +6,9 @@ from model_state import Base
 
 
 class City(Base):
-  """class definition for the City"""
-  __tablename__ = "cities"
-  id = Column(Integer, primary_key=True, autoincrement=True,
-              nullable=False)
-  name = Column(String(128), nullable=False)
-  state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    """class definition for the City"""
+
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
