acdf421eacd994f77f46c38a574db53f0689ba2d
teamkweku <teamkweku@outlook.com>
2023-09-28 14:41:03 +0000

    modifying file to conform with 3 line restriction

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-# script that takes a URL, sends a GET request to the URL
-# and displays the size of the body of the response (bytes)
-
-# Check if URL is provided
-if [  -z "$1" ]; then
-  echo "Usage: $0 URL"
-  exit 1
-fi
-
-# Send request to URL and get the size of the body in bytes
-SIZE=$(curl -s -w '%{size_download}' -o /dev/null "$1")
-
-echo "$SIZE"
+# script that takes a URL, sends a GET request to the URL and displays the size of the body of the response
+curl -s -I "$1" | awk -F ': ' '/Content-Length/ {print $2}'
