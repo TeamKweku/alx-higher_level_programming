0b1ca2dde80c65e62930ed2fa500fa01fa100f4a
teamkweku <teamkweku@outlook.com>
2023-09-29 21:21:40 +0000

    script that queries a URL and prints error or body depending on status code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a Python script that takes a URL as argument """
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
