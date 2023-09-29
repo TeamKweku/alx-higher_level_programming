2f0d42ec9aaef8dfddef5f0f05638c0d2a6e9d11
teamkweku <teamkweku@outlook.com>
2023-09-29 21:26:39 +0000

    script that queries a URL and prints error or body depending on status code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,8 +12,9 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     response = requests.get(url)
+    status = response.status_code
 
-    if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
-    else:
+    if status < 400:
         print(response.text)
+    else:
+        print(f"Error code: {status}")
