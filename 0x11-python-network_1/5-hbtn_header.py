26c3b18943a41315760fedc1e0ec90b3580071d3
teamkweku <teamkweku@outlook.com>
2023-09-29 20:12:31 +0000

    sends a request to the URL and displays the value of the variable X-Request-Id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" a Python script that takes a URL as argument """
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(f"Error: {e}")
+    else:
+        x_req_id = response.headers.get('X-Request-Id')
+        if x_req_id:
+            print(x_req_id)
+        else:
+            print("X-Request-Id Not Found")
