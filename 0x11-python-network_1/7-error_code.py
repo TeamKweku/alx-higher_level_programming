0006a191eeebd93788a9b29d8147c5103664a0c4
teamkweku <teamkweku@outlook.com>
2023-09-29 21:28:50 +0000

    Fixed checker error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <URL>")
-        sys.exit(1)
+    # if len(sys.argv) != 2:
+    #     print(f"Usage: {sys.argv[0]} <URL>")
+    #     sys.exit(1)
 
     url = sys.argv[1]
     response = requests.get(url)
