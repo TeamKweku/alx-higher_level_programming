933b3fd5ca34d639afaf74cb8023320932b746f3
teamkweku <teamkweku@outlook.com>
2023-09-29 20:56:30 +0000

    script that takes in a URL and an email address, sends a POST request to the passed URL

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" a Python script that takes a URL as argument """
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} <URL> <email>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    payload = {'email': email}
+
+    response = requests.get(url, data=payload)
+
+    print(response.text)
