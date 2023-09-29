70ca97de125ca899c8be16aecb7796694631b35f
teamkweku <teamkweku@outlook.com>
2023-09-29 16:00:39 +0000

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """ a Python script that fetches https://alx-intranet.hbtn.io/status """
-import urllib.request
 
 
-url = "https://alx-intranet.hbtn.io/status"
+if __name__ == "__main__":
+    import urllib.request
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    utf8_content = body.decode('utf-8')
+    url = "https://alx-intranet.hbtn.io/status"
 
-print("Body response:")
-print("\t-  type:", type(body))
-print("\t-  content:", repr(body))
-print("\t-  utf-8 content:", utf8_content)
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        utf8_content = body.decode('utf-8')
+
+    print("Body response:")
+    print("\t-  type:", type(body))
+    print("\t-  content:", repr(body))
+    print("\t-  utf-8 content:", utf8_content)
