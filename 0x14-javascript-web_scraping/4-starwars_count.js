7a17523202ca72169f733267a1004c3985836936
teamkweku <teamkweku@outlook.com>
2023-10-24 20:49:34 +0000

    script that prints the number of movies where the character “Wedge Antilles” is present

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// script that prints the number of movies where
+// the character “Wedge Antilles” is present
+const request = require('request');
+
+if (process.argv.length < 3) {
+  console.log('Usage: ./3-starwars_title.js <id>');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+const character = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    if (response.statusCode === 200) {
+      const movies = JSON.parse(body).results;
+      const match = movies.filter((movie) => movie.characters.includes(character));
+      console.log(match.length);
+    }
+  }
+});
