60d54b48d67303953c93f722ab83354aa0eb28e4
teamkweku <teamkweku@outlook.com>
2023-11-02 08:13:57 +0000

    fetching from an api with JSON AJAX

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,6 @@
+$(function () {
+  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
+    const characterName = data.name;
+    $('#character').text(characterName);
+  });
+});
