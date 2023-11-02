db31b161f52fc7cebffd7429e5e767f0b72e8b11
teamkweku <teamkweku@outlook.com>
2023-11-02 09:54:05 +0000

    a script that adds, removes and clears LI elements

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,14 @@
+$(function () {
+  $('#add_item').click(function () {
+    const newItem = $('<li>Item</li>');
+    $('ul.my_list').append(newItem);
+  });
+
+  $('#remove_item').click(function () {
+    $('ul.my_list li:last').remove();
+  });
+
+  $('#clear_list').click(function () {
+    $('ul.my_list').empty();
+  });
+});
